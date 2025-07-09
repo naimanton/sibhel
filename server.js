0a23795ead711fbb60771ac8dbe620a91736bdd9
naimanton <93698269+naimanton@users.noreply.github.com>
2025-07-09 11:43:08 +0500

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const PORT = 3000;
-const fs = require('fs');
-const http = require('http');
-const requestResponser = function (request, response) {
-	response.write('OK');
-	response.end();
-};
-const server = http.createServer(requestResponser);
-server.listen(PORT);
